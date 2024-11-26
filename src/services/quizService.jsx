6a3518299900c3ae5bd3d2f6dbd6576af0db8f77
Keshav Chaurasia <keshav.chaurasia1995@gmail.com>
2024-11-26 10:51:06 +0000

    fixed api

--- src/services/quizService.jsx
+++ src/services/quizService.jsx
@@ -3,6 +3,6 @@ import { ROOT_URL } from '../utils';
 
 // Function to fetch quiz questions
 export const fetchQuizQuestions = (topic) => {
-    const FETCH_URL = `${ROOT_URL}/questions?type=${topic}`;
+    const FETCH_URL = `${ROOT_URL}/questions?topic=${topic}`;
     return axios.get(FETCH_URL);
 }
